81d94cd917aa59a060fa431c7219882a46298c6e
rascarlo <rascarlo@gmail.com>
2015-12-05 15:06:27 +0000

    Volume Rocker Wake [2/2]
    Change-Id: Ib83943373ad5dabeda65466b3d4b84eb5aa3e61e
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/SoundsFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/SoundsFrag.java
@@ -27,10 +27,11 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 public class SoundsFrag extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    // volume rocker reorient
     private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
     private SwitchPreference mSwapVolumeButtons;
+    private SwitchPreference mVolumeRockerWake;
 
 
     @Override
@@ -48,6 +49,13 @@ public class SoundsFrag extends SettingsPreferenceFragment implements
         int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
                 SWAP_VOLUME_BUTTONS, 0);
         mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
+
+        //volume rocker wake
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
 
@@ -56,6 +64,11 @@ public class SoundsFrag extends SettingsPreferenceFragment implements
         if (preference == mSwapVolumeButtons) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
                     value ? 1 : 0);
             return true;
         }
