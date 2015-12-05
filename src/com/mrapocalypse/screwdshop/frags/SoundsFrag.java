99aea44cc215493129639607c4a9bfdd77491801
Nick Doherty <nickdoh81@gmail.com>
2015-12-05 13:24:57 +0100

    Forward port 'Swap volume buttons' (3/3)
    Change-Id: Ib31ece5073a757c079a8cab41a613cafd00c8326
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/SoundsFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/SoundsFrag.java
@@ -3,11 +3,13 @@ package com.mrapocalypse.screwdshop.frags;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,17 +27,39 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 public class SoundsFrag extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    // volume rocker reorient
+    private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+    private SwitchPreference mSwapVolumeButtons;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.sounds_frag);
+
+        final ContentResolver resolver = getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = getResources();
+
+        // volume rocker reorient
+        mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);
+        mSwapVolumeButtons.setOnPreferenceChangeListener(this);
+        int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
+                SWAP_VOLUME_BUTTONS, 0);
+        mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
     }
 
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwapVolumeButtons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
 
