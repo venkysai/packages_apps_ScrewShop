3b5c18baba897263080d752eb3a4b47c2f685527
Corey Edwards <ensabahnur16@gmail.com>
2016-09-08 11:01:33 -0500

    Create Statusbar fragment
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ src/com/mrapocalypse/screwdshop/frags/StatusbarFrag.java
@@ -0,0 +1,50 @@
+package com.mrapocalypse.screwdshop.frags;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+/**
+ * Created by cedwards on 6/3/2016.
+ */
+public class StatusbarFrag extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.statusbar_frag);
+    }
+
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.SCREWD;
+    }
+
+
+
+}
