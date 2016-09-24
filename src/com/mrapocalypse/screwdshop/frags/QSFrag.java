417d3e923ebfcbbe5337f472ec0381f572f84a78
Corey Edwards <ensabahnur16@gmail.com>
2016-09-24 13:52:58 -0500

    Create QSFrag
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ src/com/mrapocalypse/screwdshop/frags/QSFrag.java
@@ -0,0 +1,66 @@
+package com.mrapocalypse.screwdshop.frags;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import java.util.Date;
+
+/**
+ * Created by cedwards on 6/3/2016.
+ */
+public class QSFrag extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.qs_frag);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.SCREWD;
+    }
+
+
+
+}
