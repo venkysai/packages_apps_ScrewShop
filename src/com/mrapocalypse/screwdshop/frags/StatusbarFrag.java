c548837d7d58cd460a499a574651cadf9db2bd0b
Corey Edwards <ensabahnur16@gmail.com>
2016-09-30 15:12:24 -0500

    Revert "Statusbar clock and date customizations (2/2)"
    This reverts commit fa8171ce6ba639cff2cc36f6c76942907e853f00.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/StatusbarFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/StatusbarFrag.java
@@ -18,7 +18,6 @@ package com.mrapocalypse.screwdshop.frags;
 
 import android.content.Context;
 import android.content.ContentResolver;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -28,8 +27,7 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import android.text.format.DateFormat;
-import android.view.View;
+
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -45,21 +43,15 @@ public class StatusbarFrag extends SettingsPreferenceFragment implements
 
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
-    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
-    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
     private SwitchPreference mEnableNC;
     private SwitchPreference mForceExpanded;
-    private ListPreference mStatusBarClock;
-    private ListPreference mStatusBarAmPm;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.statusbar_frag);
-
-        ContentResolver resolver = getActivity().getContentResolver();
 
         mEnableNC = (SwitchPreference) findPreference(STATUS_BAR_NOTIF_COUNT);
         mEnableNC.setOnPreferenceChangeListener(this);
@@ -72,32 +64,11 @@ public class StatusbarFrag extends SettingsPreferenceFragment implements
         int ForceExpanded = Settings.System.getInt(getContentResolver(),
                 FORCE_EXPANDED_NOTIFICATIONS, 0);
         mForceExpanded.setChecked(ForceExpanded != 0);
-
-        mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
-        mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
-
-        int clockStyle = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CLOCK, 1);
-        mStatusBarClock.setValue(String.valueOf(clockStyle));
-        mStatusBarClock.setSummary(mStatusBarClock.getEntry());
-        mStatusBarClock.setOnPreferenceChangeListener(this);
-
-        if (DateFormat.is24HourFormat(getActivity())) {
-            mStatusBarAmPm.setEnabled(false);
-            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
-        } else {
-            int statusBarAmPm = Settings.System.getInt(resolver,
-                    Settings.System.STATUS_BAR_AM_PM, 2);
-            mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
-            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
-            mStatusBarAmPm.setOnPreferenceChangeListener(this);
-        }
     }
 
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
         if  (preference == mEnableNC) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), STATUS_BAR_NOTIF_COUNT,
@@ -108,34 +79,8 @@ public class StatusbarFrag extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), FORCE_EXPANDED_NOTIFICATIONS,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mStatusBarClock) {
-            int clockStyle = Integer.parseInt((String) newValue);
-            int index = mStatusBarClock.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, Settings.System.STATUS_BAR_CLOCK, clockStyle);
-            mStatusBarClock.setSummary(mStatusBarClock.getEntries()[index]);
-            return true;
-        } else if (preference == mStatusBarAmPm) {
-            int statusBarAmPm = Integer.valueOf((String) newValue);
-            int index = mStatusBarAmPm.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
-            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
-            return true;
         }
         return false;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Adjust clock position for RTL if necessary
-        Configuration config = getResources().getConfiguration();
-        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
-                        R.array.status_bar_clock_style_entries_rtl));
-                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
-        }
     }
 
 
