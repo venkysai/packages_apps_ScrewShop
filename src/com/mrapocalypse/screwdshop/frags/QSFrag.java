89ad67709e90f0fec1d4df821ab07bb29a2f4949
Marko Man <darkobas@gmail.com>
2016-10-07 08:58:06 +0200

    SS: allow disabling quick settings on secure lock screens [2/2]
    Change-Id: Ie218f41cbf2032a6e960f84432c4fca64fd30855
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/QSFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/QSFrag.java
@@ -38,6 +38,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.widget.LockPatternUtils;
 
 import com.mrapocalypse.screwdshop.prefs.CustomSeekBarPreference;
 
@@ -56,6 +57,7 @@ public class QSFrag extends SettingsPreferenceFragment implements
     private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
     private static final String PREF_COLUMNS = "qs_columns";
     private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+    private static final String PREF_LOCK_QS_DISABLED = "lockscreen_qs_disabled";
 
     private ListPreference mQuickPulldown;
     private ListPreference mSmartPulldown;
@@ -63,7 +65,9 @@ public class QSFrag extends SettingsPreferenceFragment implements
     private CustomSeekBarPreference mRowsLandscape;
     private CustomSeekBarPreference mQsColumns;
     private CustomSeekBarPreference mSysuiQqsCount;
+    private SwitchPreference mLockQsDisabled;
 
+    private static final int MY_USER_ID = UserHandle.myUserId();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,6 +75,8 @@ public class QSFrag extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.qs_frag);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+        final PreferenceScreen prefSet = getPreferenceScreen();
         Resources res = getResources();
         int defaultValue;
 
@@ -115,6 +121,15 @@ public class QSFrag extends SettingsPreferenceFragment implements
         mSysuiQqsCount.setValue(SysuiQqsCount / 1);
         mSysuiQqsCount.setOnPreferenceChangeListener(this);
 
+        mLockQsDisabled = (SwitchPreference) findPreference(PREF_LOCK_QS_DISABLED);
+        if (lockPatternUtils.isSecure(MY_USER_ID)) {
+            mLockQsDisabled.setChecked((Settings.Secure.getInt(resolver,
+                Settings.Secure.LOCK_QS_DISABLED, 0) == 1));
+            mLockQsDisabled.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mLockQsDisabled);
+        }
+
     }
 
 
@@ -153,6 +168,11 @@ public class QSFrag extends SettingsPreferenceFragment implements
             int SysuiQqsCount = (Integer) newValue;
             Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
+            return true;
+        } else if  (preference == mLockQsDisabled) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.LOCK_QS_DISABLED, checked ? 1:0);
             return true;
         }
         return false;
