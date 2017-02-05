9692a0b1d8ab3f8cb6e9d8d30f3c2ef1714b816d
xyyx <xyyx@mail.ru>
2017-02-05 14:47:04 +0800

    Unlock keystore with fingerprint after reboot (2/2)
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/LockscreenFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/LockscreenFrag.java
@@ -51,12 +51,14 @@ public class LockscreenFrag extends SettingsPreferenceFragment implements
 
     private static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
     private static final String FP_CAT = "fingerprint_category";
 
     private SwitchPreference mKeyguardTorch;
     private SwitchPreference mFingerprintVib;
     private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFpKeystore;
 
     PreferenceCategory fpCat;
 
@@ -83,12 +85,18 @@ public class LockscreenFrag extends SettingsPreferenceFragment implements
 
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        mFpKeystore = (SwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
         if (!mFingerprintManager.isHardwareDetected()){
             fpCat.removePreference(mFingerprintVib);
+            fpCat.removePreference(mFpKeystore);
         } else {
         mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
         mFingerprintVib.setOnPreferenceChangeListener(this);
+
+        mFpKeystore.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FP_UNLOCK_KEYSTORE, 0) == 1));
+        mFpKeystore.setOnPreferenceChangeListener(this);
         }
 
 
@@ -105,6 +113,11 @@ public class LockscreenFrag extends SettingsPreferenceFragment implements
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        } else if (preference == mFpKeystore) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FP_UNLOCK_KEYSTORE, value ? 1 : 0);
             return true;
         }
         return false;
