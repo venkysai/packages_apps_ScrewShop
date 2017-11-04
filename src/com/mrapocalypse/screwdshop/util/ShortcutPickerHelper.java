9980f50a411e1b59bee156f078a77b28b72eb877
Corey Edwards <ensabahnur16@gmail.com>
2017-11-04 16:38:38 -0500

    Un-derp all the things...
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/util/ShortcutPickerHelper.java
+++ src/com/mrapocalypse/screwdshop/util/ShortcutPickerHelper.java
@@ -38,7 +38,7 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
-import com.sos.settings.slim.util.ShortcutPickerHelper.AppExpandableAdapter.GroupInfo;
+import com.mrapocalypse.screwdshop.util.ShortcutPickerHelper.AppExpandableAdapter.GroupInfo;
 import com.android.settings.R;
 
 import com.android.internal.util.screwd.AppHelper;
