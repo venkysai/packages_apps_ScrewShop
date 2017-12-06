c1cc6077f1e904a99f64a10a4cef512488366ffc
bigrushdog <randall.rushing@gmail.com>
2017-12-05 23:50:58 -0800

    Buttons: Hardware key disable support
    Change-Id: I42f8b3b9423e218eb612bb8ff57baa58280392e6
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/mrapocalypse/screwdshop/frags/ButtonsFrag.java
+++ src/com/mrapocalypse/screwdshop/frags/ButtonsFrag.java
@@ -34,6 +34,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.internal.utils.du.ActionConstants;
+import com.android.internal.utils.du.DUActionUtils;
 
 import com.android.settings.R;
 
@@ -41,8 +42,10 @@ public class ButtonsFrag extends ActionFragment implements
         Preference.OnPreferenceChangeListener {
 
 		private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+        private static final String HWKEY_DISABLE = "hardware_keys_disable";
 
         // category keys
+        private static final String CATEGORY_HWKEY = "hardware_keys";
         private static final String CATEGORY_BACK = "back_key";
         private static final String CATEGORY_HOME = "home_key";
         private static final String CATEGORY_MENU = "menu_key";
@@ -62,6 +65,7 @@ public class ButtonsFrag extends ActionFragment implements
         public static final int KEY_MASK_VOLUME = 0x40;
 
         private ListPreference mVolumeKeyCursorControl;
+        private SwitchPreference mHwKeyDisable;
 
 
     @Override
@@ -72,6 +76,21 @@ public class ButtonsFrag extends ActionFragment implements
         final ContentResolver resolver = getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+
+        final boolean needsNavbar = DUActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(hwkeyCat);
+        }
 
         // bits for hardware keys present on device
         final int deviceKeys = getResources().getInteger(
@@ -125,6 +144,9 @@ public class ButtonsFrag extends ActionFragment implements
         // let super know we can load ActionPreferences
         onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
 
+        // load preferences first
+        setActionPreferencesEnabled(keysDisabled == 0);
+
 		// volume key cursor control
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
         if (mVolumeKeyCursorControl != null) {
@@ -144,7 +166,13 @@ public class ButtonsFrag extends ActionFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-	     if (preference == mVolumeKeyCursorControl) {
+         if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
+            return true;
+	     } else if (preference == mVolumeKeyCursorControl) {
             String volumeKeyCursorControl = (String) newValue;
             int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
             Settings.System.putInt(getActivity().getContentResolver(),
